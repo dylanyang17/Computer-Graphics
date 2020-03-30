54759941b0c566a657b1381c4819fb157ddc1001
YYR <315629555@qq.com>
2020-03-30 15:49:07 +0800

    init

---
+++ PA1.RayCasting/student-code/include/sphere.hpp
@@ -0,0 +1,61 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "object3d.hpp"
+#include <vecmath.h>
+#include <cmath>
+#include <utils.hpp>
+
+// TODO: Implement functions and add more fields as necessary
+
+class Sphere : public Object3D {
+public:
+    Sphere() {
+        // unit ball at the center
+        this->radius = 1.0f;
+        this->center = Vector3f(0.0f);
+    }
+
+    Sphere(const Vector3f &center, float radius, Material *material) : Object3D(material) {
+        // 
+        this->center = center;
+        this->radius = radius;
+    }
+
+    ~Sphere() override = default;
+
+    bool intersect(const Ray &r, Hit &h, float tmin) override {
+        // 令 A 点为射线起点， AB 为射线单位向量， O 点为球心， C 点为 O 点到 AB 的垂足
+        Vector3f pointA = r.getOrigin(), pointO = this->center;
+        Vector3f vecAB = r.getDirection().normalized(), vecAO = pointO - pointA;
+        Vector3f vecAC = vecAB * Vector3f::dot(vecAB, vecAO);
+        Vector3f vecCO = pointO - (pointA + vecAC);
+        float dis = vecCO.length();
+        if (sgn(dis - this->radius) > 0)
+            return false;
+        else if (sgn(dis - this->radius) == 0) {
+            // 相切，此时 C 点即交点
+            float t = calcT(r, pointA + vecAC);
+            if (t < tmin || t > h.getT())
+                return false;
+            h.set(t, this->material, -vecCO.normalized());
+            return true;
+        } else {
+            // 有两个交点，找最近的一个，令其为 D 点
+            // NOTE: 暂时默认射线起点在球外部
+            float lenAD = vecAC.length() - sqrt(this->radius * this->radius - vecCO.squaredLength());
+            assert(sgn(lenAD) > 0);
+            Vector3f pointD = pointA + lenAD * vecAB;
+            float t = calcT(r, pointD);
+            h.set(t, this->material, (pointD - pointO).normalized());
+            return true;
+        }
+    }
+
+protected:
+    Vector3f center;
+    float radius;
+};
+
+
+#endif
---
+++ PA1.RayCasting/student-code/include/utils.hpp
@@ -0,0 +1,22 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <cmath>
+#include <vecmath.h>
+
+int sgn(float f) {
+    if (fabs(f) < 1e-10)
+        return 0;
+    else if (f > 0)
+        return 1;
+    else
+        return -1;
+}
+
+float calcT(const Ray &r, const Vector3f &p) {
+    // 保证 p 在 r 所在直线上，计算 t 值
+    Vector3f tmpv1 = r.getDirection(), tmpv2 = p - r.getOrigin();
+    assert(sgn(Vector3f::dot(tmpv1, tmpv2) - tmpv1.length()*tmpv2.length()) == 0);
+    return tmpv2[0] / tmpv1[0];
+}
+
+#endif
