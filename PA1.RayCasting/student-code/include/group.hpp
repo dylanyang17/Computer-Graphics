69f7172fdf0d3bd8e4cd15ae2e85ce02321d7dcf
YYR <315629555@qq.com>
2020-03-30 17:10:26 +0800

    feat(group.hpp): 加入Group类，并实现了intersect方法

--- PA1.RayCasting/student-code/include/group.hpp
+++ PA1.RayCasting/student-code/include/group.hpp
@@ -19,7 +19,7 @@ public:
     }
 
     explicit Group (int num_objects) {
-
+        objects.reserve(num_objects);
     }
 
     ~Group() override {
@@ -27,19 +27,24 @@ public:
     }
 
     bool intersect(const Ray &r, Hit &h, float tmin) override {
-
+        bool ret = false;
+        for (int i = 0; i < objects.size(); ++i) {
+            ret |= objects[i]->intersect(r, h, tmin);
+        }
+        return ret;
     }
 
     void addObject(int index, Object3D *obj) {
-
+        // 下标从 0 开始
+        objects.insert(objects.begin()+index, obj);
     }
 
     int getGroupSize() {
-
+        return objects.size();
     }
 
 private:
-
+    std::vector<Object3D*> objects;
 };
 
 #endif
