16bab1ba855034090f74d71c655d0396de34e42e
yangyr17 <315629555@qq.com>
2020-05-26 21:07:02 +0800

    feat(element.hpp): Line is finished.

--- PA0.RasterGraphics/code/include/element.hpp
+++ PA0.RasterGraphics/code/include/element.hpp
@@ -21,6 +21,42 @@ public:
         // TODO: Implement Bresenham Algorithm
         printf("Draw a line from (%d, %d) to (%d, %d) using color (%f, %f, %f)\n", xA, yA, xB, yB,
                 color.x(), color.y(), color.z());
+        if (xA == xB) {
+            if (yA > yB) {
+                std::swap(yB, yB);
+            }
+            for (int y = yA; y <= yB; ++y) {
+                img.SetPixel(xA, y, color);
+            }
+        } else {
+            // 记录是否 x, y 互换
+            bool rev = (fabs((double)(yA - yB) / (xA - xB)) > 1);
+            if (rev) {
+                std::swap(xA, yA);
+                std::swap(xB, yB);
+            } 
+            if (xA > xB) {
+                std::swap(xA, xB);
+                std::swap(yA, yB);
+            }
+            // 此时一定有 |k| <= 1 且 xA <= xB
+            int dx = xB - xA, dy = yB - yA, y = yA, e = ((dy >= 0) ? -dx : dx);
+            for (int x = xA; x <= xB; ++x) {
+                img.SetPixel((rev ? y : x), (rev ? x : y), color);
+                e += 2 * dy;
+                if (dy >= 0) {
+                    if (e >= 0) {
+                        e -= 2 * dx;
+                        ++y;
+                    }
+                } else {
+                    if (e <= 0) {
+                        e += 2 * dx;
+                        --y;
+                    }
+                }
+            }
+        }
     }
 };
 
