f6bcbd814ab9a603f143bc81b93b43206f70b3a0
yangyr17 <315629555@qq.com>
2020-06-20 21:33:45 +0800

    save

--- PA1.RayCasting/code/src/main.cpp
+++ PA1.RayCasting/code/src/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
     string inputFile = argv[1];
     string outputFile = argv[2];  // only bmp is allowed.
 
-    // TODO: Main RayCasting Logic
+    // Main RayCasting Logic
     // First, parse the scene using SceneParser.
     // Then loop over each pixel in the image, shooting a ray
     // through that pixel and finding its intersection with
